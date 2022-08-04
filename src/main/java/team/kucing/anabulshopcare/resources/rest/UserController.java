2a099339da4f4347cf3f31ba24aa2177c20ff4a1
HasnulFikri <fikri98fik@gmail.com>
2022-08-04 22:04:27 +0700

    finish feature user n logger

--- src/main/java/team/kucing/anabulshopcare/resources/rest/UserController.java
+++ src/main/java/team/kucing/anabulshopcare/resources/rest/UserController.java
@@ -1,20 +1,22 @@
 package team.kucing.anabulshopcare.resources.rest;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import team.kucing.anabulshopcare.entity.UserApp;
 import team.kucing.anabulshopcare.repository.UserAppRepository;
 import team.kucing.anabulshopcare.service.impl.UserAppServiceImpl;
 
+import javax.validation.Valid;
+import java.util.UUID;
+
 @RestController
 @AllArgsConstructor
+@Slf4j
 public class UserController {
     private final UserAppServiceImpl userAppService;
     @GetMapping("/users")
@@ -32,4 +34,8 @@ public class UserController {
         return this.userAppService.signUpBuyer(userApp, file);
     }
 
+    @PutMapping(value = "/signup/update/{id}")
+    public ResponseEntity<Object> updateUser(@PathVariable(value = "id") UUID id, @RequestPart MultipartFile file, @RequestPart @Valid UserApp userApp) {
+        return this.userAppService.updateUser(userApp, file, id);
+    }
 }
--- src/main/java/team/kucing/anabulshopcare/service/UserAppService.java
+++ src/main/java/team/kucing/anabulshopcare/service/UserAppService.java
@@ -3,7 +3,10 @@ package team.kucing.anabulshopcare.service;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
+import team.kucing.anabulshopcare.entity.Product;
 import team.kucing.anabulshopcare.entity.UserApp;
+
+import java.util.UUID;
 
 public interface UserAppService {
 
@@ -12,4 +15,8 @@ public interface UserAppService {
     ResponseEntity<Object> signUpBuyer(UserApp user, MultipartFile file);
 
     ResponseEntity<Object> getAllUsers(Pageable pageable);
+
+    UserApp findById(UUID id);
+
+    ResponseEntity<Object> updateUser(UserApp user, MultipartFile file, UUID id);
 }
