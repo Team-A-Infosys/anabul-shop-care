9d36fde280b872132cf4e6d20ad797b9f387f4d6
yelliaviona <105798430+yelliaviona@users.noreply.github.com>
2022-08-08 17:01:33 +0700

    get all shipment, oke

--- src/main/java/team/kucing/anabulshopcare/resources/rest/PaymentController.java
+++ src/main/java/team/kucing/anabulshopcare/resources/rest/PaymentController.java
@@ -1,7 +1,9 @@
 package team.kucing.anabulshopcare.resources.rest;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +11,7 @@ import team.kucing.anabulshopcare.dto.request.PaymentRequest;
 import team.kucing.anabulshopcare.service.PaymentService;
 
 @RestController
+@Slf4j
 @AllArgsConstructor
 public class PaymentController {
 
@@ -17,5 +20,11 @@ public class PaymentController {
     @PostMapping("/payment-gateway/create")
     public ResponseEntity<Object> createPayment(@RequestBody PaymentRequest request) {
         return this.paymentService.createPayment(request);
+    }
+
+    @GetMapping("/payments")
+    public ResponseEntity<Object> getPayments(){
+        log.info("success get All Payments");
+        return this.paymentService.getAllPayment();
     }
 }
